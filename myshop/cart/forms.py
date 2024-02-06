5490e3d3633693fcbb8db664533b74811b52c034
Bandores <69009182+Bandores@users.noreply.github.com>
2024-02-06 14:27:31 +0600

    pizda
    suka

--- myshop/cart/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class OrderForm(forms.Form):
-    full_name = forms.CharField(label='ФИО', max_length=100)
-    city = forms.CharField(label='Адрес', max_length=100)
-    phone = forms.CharField(label='Телефон', max_length=15)
