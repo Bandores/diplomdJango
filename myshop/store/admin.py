80ae79b245208ce00e5d57cbb11fc8a5fef9310d
Bandores <69009182+Bandores@users.noreply.github.com>
2024-02-20 19:26:13 +0600

    new data

--- myshop/store/admin.py
+++ myshop/store/admin.py
@@ -2,8 +2,20 @@ from django.contrib import admin
 from .models import Category, Product,Cart, CartItem,Purchase
 app_name = 'store'
 
-admin.site.register(Category)
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']  # Поля для отображения в таблице
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category_name', 'price', 'img', 'total']  # Поля для отображения в таблице
+    list_filter = ['category']  # Добавление фильтрации по категории
+
+    def category_name(self, obj):
+        return obj.category.name
+
+    category_name.short_description = 'Категория'
+
 admin.site.register(CartItem)
 admin.site.register(Cart)
 admin.site.register(Purchase)
